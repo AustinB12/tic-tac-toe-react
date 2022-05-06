d3eb1a3080444e6cd0e190821492c65ebcf03b61
Austin Baird <42442591+AustinB12@users.noreply.github.com>
2022-05-06 14:43:48 -0400

    Game Final Form + Navigable History

--- src/Board.js
+++ src/Board.js
@@ -1,85 +1,16 @@
 import React from 'react'
-import {
-  useLocalStorageState,
-  calculateNextValue,
-  calculateWinner,
-  calculateStatus,
-} from './utils'
 
-const Board = () => {
-  const [squares, setSquares] = useLocalStorageState(
-    'squares',
-    Array(9).fill(null),
-  )
-  const [history, setHistory] = useLocalStorageState('history', [])
-
-  const nextValue = calculateNextValue(squares)
-  const winner = calculateWinner(squares)
-  const status = calculateStatus(winner, squares, nextValue)
-
-  function selectSquare(square) {
-    if (winner || squares[square]) {
-      return
-    }
-    const squaresCopy = [...squares]
-    squaresCopy[square] = nextValue
-    setSquares(squaresCopy)
-    setHistory([...history, squaresCopy])
-  }
-
-  function restart() {
-    setSquares(Array(9).fill(null))
-    setHistory([])
-  }
-
+const Board = ({ onClick, squares }) => {
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
   }
 
-  function renderHistory() {
-    console.log('history.length', history.length)
-    const shouldRenderHistory = history.length > 0
-
-    if (!shouldRenderHistory) {
-      return
-    }
-
-    const histories = history.map((squares, index) => {
-      return (
-        <div key={index} style={{ margin: '10px 10px' }}>
-          <div className="board-row">
-            {renderHistorySquare(index, 0)}
-            {renderHistorySquare(index, 1)}
-            {renderHistorySquare(index, 2)}
-          </div>
-          <div className="board-row">
-            {renderHistorySquare(index, 3)}
-            {renderHistorySquare(index, 4)}
-            {renderHistorySquare(index, 5)}
-          </div>
-          <div className="board-row">
-            {renderHistorySquare(index, 6)}
-            {renderHistorySquare(index, 7)}
-            {renderHistorySquare(index, 8)}
-          </div>
-        </div>
-      )
-    })
-
-    return histories
-  }
-
-  function renderHistorySquare(i, j) {
-    return <button className="square">{history[i][j]}</button>
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -95,10 +26,6 @@ const Board = () => {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
-      <div className="history-container">{renderHistory()}</div>
     </div>
   )
 }
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,61 @@
+import React from 'react'
 import { Board } from './Board'
+import {
+  useLocalStorageState,
+  calculateNextValue,
+  calculateWinner,
+  calculateStatus,
+} from './utils'
 
 export const Game = () => {
+  const [currentStep, setCurrentStep] = React.useState(0)
+  const [history, setHistory] = useLocalStorageState('history', [
+    Array(9).fill(null),
+  ])
+  const currentSquares = history[currentStep]
+
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) {
+      return
+    }
+    const newHistory = history.slice(0, currentStep + 1)
+    const squaresCopy = [...currentSquares]
+    squaresCopy[square] = nextValue
+    setHistory([...newHistory, squaresCopy])
+    setCurrentStep(newHistory.length)
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }
+
+  let moves = history.map((stepSquares, step) => {
+    const description = step === 0 ? 'Go to game start' : `Go to move #${step}`
+    const isCurrentStep = step === currentStep
+    return (
+      <li key={step}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+          {description} {isCurrentStep ? '(current)' : null}
+        </button>
+      </li>
+    )
+  })
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={() => restart()}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
